f3e815491fe7505026ac86f7e09b13081b0b24d3
Zachary Champion <zchampion270@gmail.com>
2017-09-07 17:11:41 -0600

    Added the main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/*
+* Programmer:         Zachary Champion
+* Project:            Project Code Diva
+* Date Last Updated:  7 September 2017
+*/
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
