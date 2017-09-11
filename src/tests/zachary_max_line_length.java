e0912dfdadcc4e9a11e0ca75b245e782ac283a3f
Zachary Champion <zchampion270@gmail.com>
2017-09-10 20:09:10 -0600

    Cleared my custom tests for compile errors.

--- src/tests/zachary_max_line_length.java
+++ src/tests/zachary_max_line_length.java
@@ -4,7 +4,7 @@ public class zachary_max_line_length
 {
    public static void main(String[] args)
    {
-      System.out.println("In the face of overwhelming odds, I'm left with only one option, I'm gonna have to science the shit out of this.");
+      System.out.println("\"In the face of overwhelming odds, I'm left with only one option. I'm gonna have to science the shit out of this.\"");
       System.out.println();
       System.out.println("Said by Mark Watney in The Martian by Andy Weir.");
    }
