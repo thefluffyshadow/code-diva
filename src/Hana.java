51f59add8a9a1e382943546706c380aafbe7cb19
Zachary Champion <zchampion270@gmail.com>
2017-09-11 13:31:02 -0600

    Added a Tracer variable so that when Tracer is turned on, Hana will give diagnostic messages.

--- src/Hana.java
+++ src/Hana.java
@@ -6,18 +6,18 @@
 
 public class Hana {
 
-   boolean Tracer = true;
+   private static boolean Tracer = true;
 
    public static void main(String[] args) {
 
       // List the arguments passed in if there are any to confirm test receipt.
-      if (args.length > 0) {
+      if ((Tracer) && (args.length > 0)) {
          System.out.println("..." + args.length + " args passed...");
          for (String arg : args) {
             System.out.println("...arg passed:  \"" + arg + "\"");
          }
 
-      } else {
+      } else if (Tracer) {
          System.out.println("...no args passed.");
       }
 
