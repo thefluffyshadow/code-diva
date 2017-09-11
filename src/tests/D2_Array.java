6b231fab1b803f4932293d67da2a9f25582604c2
Zac Champion <zchampion270@gmail.com>
2017-09-10 18:08:18 -0600

    Minor adjustment.

--- src/tests/D2_Array.java
+++ src/tests/D2_Array.java
@@ -12,8 +12,8 @@ public class D2_Array
 {
 	public static void main(String[] args) throws Exception
     {
-       for (int y = 0; y < 8; y++)
-          for (int x = 0; x < 8; x++)
+       for (int y = 0; y <= 8; y++)
+          for (int x = 0; x <= 8; x++)
              System.out.println(x + " * " + y + " = " + x * y);
 	}
 }
