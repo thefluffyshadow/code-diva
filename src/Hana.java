c5414fa06a312b5359be5faa2548a9d9f0fed856
Zachary Champion <zchampion270@gmail.com>
2017-09-11 13:14:38 -0600

    Added a diagnostic message to tell what arguments were passed in to Hana.

--- src/Hana.java
+++ src/Hana.java
@@ -1,11 +1,25 @@
 /*
 * Programmer:         Zachary Champion
 * Project:            Project Code Diva
-* Date Last Updated:  10 September 2017
+* Date Last Updated:  11 September 2017
 */
 
 public class Hana {
-    public static void main(String[] args) {
-	// write your code here
-    }
+
+   boolean Tracer = true;
+
+   public static void main(String[] args) {
+
+      // List the arguments passed in if there are any to confirm test receipt.
+      if (args.length > 0) {
+         System.out.println("..." + args.length + " args passed...");
+         for (String arg : args) {
+            System.out.println("...arg passed:  \"" + arg + "\"");
+         }
+
+      } else {
+         System.out.println("...no args passed.");
+      }
+
+   }
 }
